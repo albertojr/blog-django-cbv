e1563037da19a64061f7cf9ea317c9be86ca971c
albertojr <alberto.crato13@gmail.com>
2021-05-15 09:46:17 -0300

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.BlogListView.as_view(),name='home'),
+    path('post/<int:pk>/', views.BlogDetailView.as_view(),name='post_detail'),
+    path('post/new/', views.BlogCreateView.as_view(),name='post_new'),
+    path('post/<slug:slug>/edit/', views.BlogUpdateView.as_view(),name='post_edit'),
+    path('post/<slug:slug>/delete/', views.BlogDeleteView.as_view(),name='post_delete'),
+]
---
+++ blog/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView,DetailView,UpdateView,DeleteView
+from django.views.generic.edit import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+
+from .forms import Postform
+
+from . models import Post
+# Create your views here.
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'custom'
+
+class BlogCreateView(SuccessMessageMixin,CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    form_class = Postform
+    success_message = "%(field)s - criado com sucesso!"
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.autor = self.request.user
+        obj.save()
+        return super().form_valid(form)
+
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+        cleaned_data,
+        field=self.object.titulo,
+    )
+
+class BlogUpdateView(SuccessMessageMixin,UpdateView):
+    model = Post
+    template_name = 'blog/post_edit.html'
+    fields = ('titulo','conteudo')
+    
+    def get_success_message(self, cleaned_data):
+        return self.success_message % dict(
+            cleaned_data,
+            field=self.object.titulo,
+        )
+
+class BlogDeleteView(SuccessMessageMixin,DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('home')
+    success_message = "Deletado com sucesso!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super(BlogDeleteView,self).delete(request,*args, **kwargs)
